0b0ffbe0645dede6823061c380aad9df17ebfd46
juliorg99 <juliorg99@gmail.com>
2015-03-10 01:04:39 -0500

    inicio sesion

--- EliteParkingXamarinDroid/MainActivity.cs
+++ EliteParkingXamarinDroid/MainActivity.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Net;
+using System.IO;
 using System.Json;
+using System.Threading.Tasks;
 
 namespace EliteParkingXamarinDroid
 {
@@ -27,23 +29,54 @@ namespace EliteParkingXamarinDroid
 
 			iniciar.Click += async (sender, e) => {
 
-				string url = "http://eliteparkingapp.com?cedula=" +
+				string url = "http://eliteparkingapp.com/login.php?cedula=" +
 					cedula.Text +
-					"?pass=" +
+					"&contrasena=" +
 					pass.Text;
 
-				System.Console.WriteLine(url); 
-				//JsonValue json = await FetchWeatherAsync (url);
+				System.Console.WriteLine(url);
+				JsonValue json = await FetchWeatherAsync (url);
+				ParseAndDisplay (json);
 			};
 
-			Button registrar = FindViewById<Button> (Resource.Id.registrar);
-			
+			Button registrar = FindViewById<Button> (Resource.Id.registrar);			
 			registrar.Click += (sender, e) =>
 			{
 				var intent = new Intent(this, typeof(RegistroActivity));
 				StartActivity(intent);
 			};
 		}
+
+		private async Task<JsonValue> FetchWeatherAsync (string url)
+		{
+			// Create an HTTP web request using the URL:
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (url));
+			request.ContentType = "application/json";
+			request.Method = "GET";
+
+			// Send the request to the server and wait for the response:
+			using (WebResponse response = await request.GetResponseAsync ())
+			{
+				// Get a stream representation of the HTTP web response:
+				using (Stream stream = response.GetResponseStream ())
+				{
+					// Use this stream to build a JSON document object:
+					JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
+					Console.Out.WriteLine("Response: ", jsonDoc.ToString ());
+
+					// Return the JSON document:
+					return jsonDoc;
+				}
+			}
+		}
+
+		private void ParseAndDisplay (JsonValue json)
+		{
+			TextView estado = FindViewById<TextView>(Resource.Id.estado);
+
+			estado.Text = "idUsuarioApp";
+		}
+
 	}
 }
 
--- EliteParkingXamarinDroid/Resources/Resource.designer.cs
+++ EliteParkingXamarinDroid/Resources/Resource.designer.cs
@@ -66,35 +66,38 @@ namespace EliteParkingXamarinDroid
 			// aapt resource value: 0x7f050000
 			public const int INcedula = 2131034112;
 			
-			// aapt resource value: 0x7f05000b
-			public const int button1 = 2131034123;
+			// aapt resource value: 0x7f05000c
+			public const int button1 = 2131034124;
 			
-			// aapt resource value: 0x7f050004
-			public const int editText1 = 2131034116;
-			
-			// aapt resource value: 0x7f050009
-			public const int editText2 = 2131034121;
+			// aapt resource value: 0x7f050005
+			public const int editText1 = 2131034117;
 			
 			// aapt resource value: 0x7f05000a
-			public const int editText3 = 2131034122;
+			public const int editText2 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int editText3 = 2131034123;
+			
+			// aapt resource value: 0x7f050004
+			public const int estado = 2131034116;
 			
 			// aapt resource value: 0x7f050002
 			public const int inicio = 2131034114;
 			
-			// aapt resource value: 0x7f050007
-			public const int papellido = 2131034119;
+			// aapt resource value: 0x7f050008
+			public const int papellido = 2131034120;
 			
-			// aapt resource value: 0x7f050005
-			public const int pnombre = 2131034117;
+			// aapt resource value: 0x7f050006
+			public const int pnombre = 2131034118;
 			
 			// aapt resource value: 0x7f050003
 			public const int registrar = 2131034115;
 			
-			// aapt resource value: 0x7f050008
-			public const int sapellido = 2131034120;
+			// aapt resource value: 0x7f050009
+			public const int sapellido = 2131034121;
 			
-			// aapt resource value: 0x7f050006
-			public const int snombre = 2131034118;
+			// aapt resource value: 0x7f050007
+			public const int snombre = 2131034119;
 			
 			static Id()
 			{
