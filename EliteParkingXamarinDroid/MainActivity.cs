097f6b95c4c47cb9772fe3cadd14ffb1f77d7379
juliorg99 <juliorg99@gmail.com>
2015-03-09 22:00:35 -0500

    layout application

---
+++ EliteParkingXamarinDroid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace EliteParkingXamarinDroid
+{
+	[Activity (Label = "EliteParkingXamarinDroid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button registrar = FindViewById<Button> (Resource.Id.registrar);
+			
+			registrar.Click += (sender, e) =>
+			{
+				var intent = new Intent(this, typeof(RegistroActivity));
+				StartActivity(intent);
+			};
+		}
+	}
+}
+
+
---
+++ EliteParkingXamarinDroid/Resources/layout/MyView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace EliteParkingXamarinDroid
+{
+	public class MyView : ContentView
+	{
+		public MyView ()
+		{
+			Content = new Label { Text = "Hello ContentView" };
+		}
+	}
+}
+
+
